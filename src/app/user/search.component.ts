da20f0831bf2231c9498508db360799c1308e329
Nelson Kimani <nelsonkimani96@gmail.com>
2019-09-28 16:38:07 +0300

    Add a find user method to get user name

--- src/app/user/search.component.ts
+++ src/app/user/search.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { Repository } from '../repository';
+import { SearchRequestService } from '../search-service/search-request.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  public searchMe = 'AceNel';
+  public githubUser: string;
+
+  users: User;
+  repository: Repository;
+  public searchRepo: string;
+  public resultCount = 12;
+
+
+  //find user function taking a username argument
+  findUser(username) {
+    this.githubUser = '';
+    this.searchMe = username;
+    this.ngOnInit();
+  }
+
+
+  constructor(public githubUserRequest: SearchRequestService, public userRepos: SearchRequestService) { }
 
   ngOnInit() {
+    this.githubUserRequest.githubUser(this.searchMe);
+    this.users = this.githubUserRequest.users;
+    this.userRepos.gitUserRepos(this.searchMe);
+    console.log(this.userRepos);
+  }
+  searchRepos() {
+    this.searchRepo = '';
+    this.resultCount = 10;
+
   }
 
 }
