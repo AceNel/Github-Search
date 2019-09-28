6b10a9c49fe98b1b9d60463874db70abeff18ab6
Nelson Kimani <nelsonkimani96@gmail.com>
2019-09-28 16:52:05 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchComponent } from './user/search.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { CommonModule } from '@angular/common';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: SearchComponent},
+  {path: 'repository', component: RepositoriesComponent},
+  {path: '', redirectTo: '/users', pathMatch: 'full'},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+      RouterModule.forRoot(routes)
+  ],
+  declarations: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
